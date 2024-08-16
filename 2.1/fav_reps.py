a3e7c80790187e9730dfe7334e5cbcb173d88df6
anastasia20216 <136759273+anastasia20216@users.noreply.github.com>
2024-08-16 21:06:54 +1200

    Add files via upload

---
+++ 2.1/fav_reps.py
@@ -0,0 +1,151 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import sqlite3
+import subprocess
+import tkinter as tk
+import os
+from tkinter import messagebox
+root = Tk()
+root.title("Recipe")
+root.geometry("1600x900")
+root.config(bg='#F9F9F9')
+def show_recipes():
+    subprocess.run(["python", "recipes.py"])
+    root.destroy()
+def show_fav():
+    root.destroy()
+    subprocess.run(["python", "fav_reps.py"])
+def search():
+    root.destroy()
+    subprocess.run(["python", "search2.py"])
+def home():
+    root.destroy()
+    subprocess.run(["python", "app3.py"])
+def profile():
+    root.destroy()
+    subprocess.run(["python", "user_pg.py"])
+def login():
+    root.destroy()
+    subprocess.run(["python", "login.py"])     
+ #This function gets called when a recipe button is clicked
+def BtnClicked(rep_button, idx, rep_name):
+    global selected_id
+    selected_id = id_list[idx]
+    print(idx)
+    print(f"recipe id {selected_id} recipe name: {rep_name[idx]}")
+    os.environ['id'] = str(selected_id)
+    root.destroy()
+    subprocess.run(["python", "rep_fav_feature_test.py"])
+
+
+
+# Database Connection and Data Retrieval
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+query_fav = "SELECT saved_rep FROM users WHERE active_user='yes'"
+cursor.execute(query_fav)
+row = cursor.fetchone()
+
+if row == "":
+    no_saved = Label(root, text="No recipes saved", bg='#FAF9F6', font=("Helvetica", 20, "bold"))
+    no_saved.place(relx=0.3, rely=0.12)
+if row:
+    # row is a tuple; get the first element (the string)
+    list_string = row[0].strip() if row[0] else ''
+    if list_string == "":
+        no_saved = Label(root, text="No recipes saved", bg='#FAF9F6', font=("Helvetica", 20, "bold"))
+        no_saved.place(relx=0.18, rely=0.25)
+    # Split the string into a list of strings
+    str_values = list_string.split(', ')
+        
+        # Optionally, convert the list of strings to a list of integers
+    id_list = [int(value) for value in str_values if value.isdigit()]
+        
+    print('Original Data:', list_string)
+    print('Split List of Values:', str_values)
+    print('List of Integers:', id_list)
+else:
+        # If the query returns None, get the user to log back in
+    messagebox.showwarning(
+        "Login Required",
+        "You are not logged in. Please log in again."
+        )
+    login()
+
+# Create placeholders for the SQL query
+placeholders = ', '.join(['?'] * len(id_list))
+
+# Create a CASE statement to order by the original list order
+order_by_case = ' '.join([f"WHEN ? THEN {i}" for i in range(len(id_list))])
+
+# SQL query that keeps the order of the original ID list
+query = f"""
+SELECT * FROM recipe
+WHERE id IN ({placeholders})
+ORDER BY CASE id {order_by_case} END
+"""
+
+# Execute the query with the list of IDs twice (for WHERE and ORDER BY)
+cursor.execute(query, id_list + id_list)
+rows = cursor.fetchall()
+
+# Assuming the 5th column contains image paths
+rep_img_paths = [row[5] for row in rows]
+connection.close()
+# Extract the names based on the column index
+column_index = 1 
+rep_name = [row[column_index] for row in rows]
+
+# Load Images using PIL and ImageTk
+image_refs = []
+for img_path in rep_img_paths:
+    try:
+        path = img_path
+        base ='C:/Users/anast/OneDrive/Documents/computer science/L3 Python/Development Log/2.0/images'
+        full_path = os.path.join(base, img_path)
+        img = Image.open(full_path)
+        img = img.resize((225,150))  #Resize
+        photo = ImageTk.PhotoImage(img)
+        image_refs.append(photo)
+    except Exception as e:
+        print(f"Error loading image {img_path}: {e}")
+        # Handle error, maybe use a default image
+
+
+nav_bar = Frame(root, bg='#F5F2EB')
+nav_bar.place(relx=0, rely=0, relheight=0.11, relwidth=1)
+
+label = Label(nav_bar, text='RECIPES', font=('Times', 30, 'bold'), bg='#F5F2EB')
+label.place(relx=0.8, rely=0.25)
+
+Button(nav_bar, text='Home', relief="flat", borderwidth=0, command=home, width=10, height=2, justify='center', bg='#FAF9F6', activebackground='white', font="Times, 12").place(relx=0.03, rely=0.25)
+Button(nav_bar, text='Search', relief="flat", borderwidth=0, command=search, width=10, height=2, justify='center', bg='#FAF9F6', activebackground='white', font="Times, 12").place(relx=0.13, rely=0.25)
+Button(nav_bar, text="Favorites", relief="flat", borderwidth=0, command=show_fav, width=10, height=2, justify='center', bg='#FAF9F6', activebackground='white', font="Times, 12").place(relx=0.23, rely=0.25)
+Button(nav_bar, text="Profile", relief="flat", borderwidth=0, command=profile, width=10, height=2, justify='center', bg='#FAF9F6', activebackground='white', font="Times, 12").place(relx=0.33, rely=0.25)
+
+Tilte = Label(root, text="Favorites", font=("Helvetica", 20, "bold"), bg='#FAF9F6')
+Tilte.place(relx=0.15, rely=0.175)
+
+
+x = 0.18 
+y = 0.25
+buttons = []
+
+for i in range(12):
+    if i < len(rep_name):
+        idx = i
+        rep_button = tk.Button(root, image=image_refs[i], text=rep_name[i], relief="flat", borderwidth=0, compound=TOP, bg='#F5F2EB', font=("Helvetica", 10), padx=5, pady=5)
+        rep_button.place(relx=x, rely=y)
+        x += 0.2
+        rep_button.config(command=lambda b=rep_button, id=idx, v=rep_name: BtnClicked(b, id, v))
+        buttons.append(rep_button)
+        if i == 3:
+            y += 0.25
+            x = 0.18
+
+        if i == 7:
+            y += 0.25
+            x = 0.18
+           
+
+root.mainloop()
---
+++ 2.1/loginen.py
@@ -0,0 +1,85 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+import subprocess
+import bcrypt
+
+# Function to validate login credentials
+def validate_login():
+    username = username_entry.get()
+    password = password_entry.get()
+    
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute("UPDATE users SET active_user = NULL")
+    conn.commit()
+    c.execute("SELECT * FROM users WHERE username=?", (username))
+    
+    result = c.fetchone()
+    stored_hash = result[2]
+    def verify_password(stored_hash, password):
+    # Check if the given password matches the stored hash
+        return bcrypt.checkpw(password.encode('utf-8'), stored_hash)
+# Test the function
+    password_to_check = "my_secret_password"
+    if verify_password(password, password_to_check):
+        print("Password is correct.")
+    else:
+        print("Password is incorrect.")
+    
+    if result:
+        c.execute("UPDATE users SET active_user = 'yes' WHERE username=?", (username))
+        conn.commit()
+        conn.close()
+
+        messagebox.showinfo("Login Success", "Welcome!")
+        root.destroy()
+        subprocess.run(["python", "app3.py"]) 
+    else:
+        conn.commit()
+        conn.close()
+        messagebox.showerror("Login Failed", "Invalid username or password")
+
+def sign_up():
+    root.destroy()
+    subprocess.run(["python", "sign_up.py"])
+
+
+# Initialize the main window
+root = Tk()
+root.title("Login Page")
+root.geometry("400x450")
+root.configure(background='#FAF9F6')
+fr = Frame(root, )
+
+
+
+#Title
+label = Label(root, text='RECIPES', font=('Times', 30, 'bold'), bg='#FAF9F6')
+label.grid(row=1, column=0, columnspan=2, padx=5, pady=50, sticky='nesw')
+# Create and place the username label and entry
+username_label = Label(root, text="Username:", background='#FAF9F6')
+username_label.grid(row=2, column=0, padx=10, pady=10, sticky='e')
+username_entry = Entry(root)
+username_entry.grid(row=2, column=1, padx=5, pady=10, sticky='w')
+
+# Create and place the password label and entry
+password_label = Label(root, text="Password:", background='#FAF9F6')
+password_label.grid(row=3, column=0, padx=10, pady=10, sticky='e')
+password_entry = Entry(root, show="*")
+password_entry.grid(row=3, column=1, padx=5, pady=15, sticky='w')
+
+# Create and place the login button
+login_button = Button(root, text="Login", command=validate_login, borderwidth=0.5, bg='#FAF9F6')
+login_button.grid(row=4, column=0, columnspan=2, pady=10)
+
+# Create and place the registration button
+registration_button = Button(root, text="Sign up", command=sign_up, borderwidth=0.5, bg='#FAF9F6')
+registration_button.grid(row=5, column=0, columnspan=2, pady=5)
+
+# Adjust grid weights to center the content
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+# Start the Tkinter event loop
+root.mainloop()
